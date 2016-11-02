e516c45e4737ce6a14b66f06f17d19b564188b41
bangprovn <bangprovn@gmail.com>
2016-11-02 14:24:03 +0700

    UOS: initialized

---
+++ config.mk
@@ -0,0 +1,30 @@
+
+#DU Utils Library
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
+ 
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
+DEVICE_PACKAGE_OVERLAYS += vendor/uos/overlay/common
+
+# crDroid Maintainers list
+PRODUCT_COPY_FILES += \
+    vendor/uos/Maintainers.txt:system/etc/Maintainers.txt
+
+# Product version should match Android version
+PRODUCT_VERSION_MAJOR = 0
+PRODUCT_VERSION_MINOR = 1
+
+# Increase UOS Version with each major release.
+UOS_VERSION := 0.1
+
+CM_VERSION := UOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+CM_DISPLAY_VERSION := UOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-v$(CR_VERSION)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.uos.version=$(CM_VERSION) \
+    ro.modversion=$(CM_VERSION)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.uos.display.version=$(CM_DISPLAY_VERSION)
